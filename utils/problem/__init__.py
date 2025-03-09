1a77d4c5ad94828150cce623e0a043bacf4583ce
Sverre.Eriksen <sverre.eriksen@student.uib.no>
2025-03-09 07:23:33 +0100

    Add __init__.py file to make utils.problem a package

---
+++ utils/problem/__init__.py
@@ -0,0 +1,9 @@
+"""Module for loading problem instances.
+
+Functions:
+    load_problem(path: str) -> Tuple[Set[Vehicle], Set[Call]]
+"""
+
+from utils.problem.problem_utils import load_problem
+
+__all__ = ["load_problem"]
