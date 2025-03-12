06347846ec9e08de0030d32bfb3b22f4593c19a0
Sverre.Eriksen <sverre.eriksen@student.uib.no>
2025-03-12 21:57:37 +0100

    Add frontier logic to State class

--- models/state.py
+++ models/state.py
@@ -2,7 +2,7 @@
 This module implements the State class which is used for search based solvers.
 """
 
-from typing import Dict, FrozenSet, Optional, List, TYPE_CHECKING
+from typing import Dict, FrozenSet, Set, Optional, List, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from models.actions import Action, Call, Delivery
@@ -25,24 +25,12 @@ class State:
         self.selected_action = None
         self.next_state = None
 
-    def delete_children(self):
-        """Deletes all children recursively. Resets selected_action and next_state."""
-        for next_state in self.next_states.values():
-            next_state.delete_children()
+        self._frontier : Set["State"] = set()
 
-        self.next_states.clear()
-        self.selected_action = None
-        self.next_state = None
-
-    def action_sequence(self) -> List["Action"]:
-        """
-        Returns the sequence of actions from this state, to the final \\
-        state with no selected action.
-        """
-        if self.selected_action is None:
-            return []
-
-        return [self.selected_action] + self.next_state.action_sequence()
+    def add_transition(self, action : "Action", next_state : "State"):
+        """Adds (action, next_state) to self.next_states"""
+        self.next_states[action] = next_state
+        self._frontier.add(next_state)
 
     def select(self, action : "Action"):
         """Selects the provided action if in self.current_state.next_states"""
@@ -51,6 +39,7 @@ class State:
 
         self.selected_action = action
         self.next_state = self.next_states[action]
+        self._frontier = self.next_state.local_frontier
 
     def remove(self, call: "Call"):
         """Removes all occurrences of call in the tree."""
@@ -60,8 +49,44 @@ class State:
         for action in to_remove:
             del self.next_states[action]
 
-        for next_state in self.next_states.values():
-            next_state.remove(call)
+        for state in self.next_states.values():
+            state.remove(call)
+
+        self._frontier = set().union(self.next_states.values())
+
+    @property
+    def frontier(self) -> Set["State"]:
+        """
+        Returns frontier of State.
+        Obtains frontier recursively.
+        Updates Frontier on calculation.
+        """
+        new_frontier = set()
+        for state in self.local_frontier:
+            if not state.next_states:
+                new_frontier.add(state)
+                continue
+            new_frontier.update(state.frontier)
+
+        self._frontier = new_frontier
+
+        return new_frontier
+
+    @property
+    def local_frontier(self) -> Set["State"]:
+        """Returns the locally stored frontier without triggering full recomputation."""
+        return self._frontier if self.next_states else {self}
+
+    @property
+    def action_sequence(self) -> List["Action"]:
+        """
+        Returns the sequence of actions from this state, to the final \\
+        state with no selected action.
+        """
+        if self.selected_action is None:
+            return []
+
+        return [self.selected_action] + self.next_state.action_sequence
 
     def __repr__(self) -> str:
         return self.__str__()
